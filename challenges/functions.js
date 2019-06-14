3dace6e5d18135d069b3c44576d332aeb5b3e6f3
adriancobon <adriancobon@outlook.com>
2019-06-14 11:47:10 -0400

    Completed the function.js challenge/

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume (c, d, cb) {
+  return cb(c, d);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +18,17 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add (a, b) {return a + b;}
+function multiply (a, b) {return a * b;}
+function greeting(fN, lN) {
+  return `Hello ${fN} ${lN}, nice to meet you!`;
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,7 +36,7 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+// myFunction's only local variable is inside the function, even though it calls an external. The reason it can do this is that it has access to all variables in the global scope. The nested function has no local variables/parameters and can call variables created above it in the JavaScript food chain. 
 
 const external = "I'm outside the function";
 
